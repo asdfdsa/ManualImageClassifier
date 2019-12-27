46a45b9c2fde396b6e75c029aeabbbd3339434c5
binwang <bin.wang@vironova.com>
2019-12-27 12:53:05 +0100

    updated info in setup.py and gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup
 
 setup(
+	name="manualLabeler",
+    version="0.0.1",
+    description="Manual image classifier for purposes such as deep learning",
+
+    author="Bin Wang",
+    author_email="alexwang911217@gmail.com",
+    url="https://https://github.com/asdfdsa/ManualImageClassifier",
+
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    install_requires=["numpy", "matplotlib"],
+
     entry_points={
         'console_scripts': [
             # main
